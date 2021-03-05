ee5b936b97cd0c213e8e9973ac087a1ed14bba1a
Brandon Smith <bsmith@republicwireless.com>
2021-03-04 22:57:55 -0500

    Hidden reset command to blow away storage

---
+++ src/commands/reset.ts
@@ -0,0 +1,13 @@
+import { Command } from '../lib/command'
+
+const debug = require(`debug`)(`reset`)
+
+export default class Reset extends Command {
+  static hidden = true
+  static description = 'reset Relay CLI configuration'
+
+  async run() {
+    this.relay.reset()
+    this.log(`Relay CLI configuration cleared`)
+  }
+}
