0a99b49bd29231f74143d9dc68635a379c1ca10a
Brandon Smith <bsmith@relaypro.com>
2022-03-31 17:04:40 -0400

    First experiment with `enableJsonFlag` on command

--- src/commands/env.ts
+++ src/commands/env.ts
@@ -1,8 +1,18 @@
 import { CliUx } from '@oclif/core'
 import { Command } from '../lib/command'
+// import { string } from '../lib/flags'
 import { ROOT_DOMAIN, vars } from '../lib/vars'
 
+type EnvResult = {
+  env: string,
+  api: string,
+  auth: string,
+  auth_cli_id: string,
+  auth_sdk_id: string,
+}
+
 export default class Env extends Command {
+  static enableJsonFlag = true
   static description = `displays the configured environment`
   static examples = [`
 Define API and Auth Hosts using shell environment variables:
@@ -16,17 +26,23 @@ RELAY_HOST=all-api-qa-ibot.${ROOT_DOMAIN}    # default
 RELAY_HOST=all-main-pro-ibot.${ROOT_DOMAIN}
 `.trim()]
 
-  async run(): Promise<void> {
-    CliUx.ux.styledHeader(`ENVIRONMENT`)
-    CliUx.ux.styledObject({
+  async run(): Promise<EnvResult> {
+    const result = {
       env: vars.env,
       api: vars.host,
       auth: vars.authHost,
       auth_cli_id: vars.authCliId,
       auth_sdk_id: vars.authSdkId,
-    })
-    this.log(``)
-    CliUx.ux.styledHeader(`USAGE`)
-    Env.examples.forEach(e => this.log(e))
+      message: `hello world`
+    }
+
+    if (!this.jsonEnabled()) {
+      CliUx.ux.styledHeader(`ENVIRONMENT`)
+      CliUx.ux.styledObject(result)
+      this.log(``)
+      CliUx.ux.styledHeader(`USAGE`)
+      Env.examples.forEach(e => this.log(e))
+    }
+    return result
   }
 }
