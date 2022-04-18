8a556f157c32d376e042a4fb78ff8025f42e5972
Brandon Smith <bsmith@relaypro.com>
2022-04-18 14:16:51 -0400

    Enable —json on `whoami` command

--- src/commands/whoami.ts
+++ src/commands/whoami.ts
@@ -4,21 +4,25 @@ import { Command } from '../lib/command'
 
 export default class AuthWhoami extends Command {
   static description = `display the current logged in user`
+  static enableJsonFlag = true
 
-  async run(): Promise<void> {
+  async run(): Promise<Record<string, unknown>> {
     if (process.env.RELAY_API_KEY) this.warn(`RELAY_API_KEY is set`)
     if (!this.relay.auth) this.notloggedin()
     try {
       const iam = await this.relay.whoami()
-      CliUx.ux.styledHeader(`You are`)
-      CliUx.ux.styledObject(iam, [
-        `Name`,
-        `Email`,
-        `Default Subscriber`,
-        `Auth User ID`,
-        `Relay User ID`,
-        `Capabilities`,
-      ])
+      if (!this.jsonEnabled()) {
+        CliUx.ux.styledHeader(`You are`)
+        CliUx.ux.styledObject(iam, [
+          `Name`,
+          `Email`,
+          `Default Subscriber`,
+          `Auth User ID`,
+          `Relay User ID`,
+          `Capabilities`,
+        ])
+      }
+      return iam
     } catch (error) {
       if (error instanceof HTTPError) {
         if (error.statusCode === 401) this.notloggedin()
