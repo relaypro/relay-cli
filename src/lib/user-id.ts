8e99eabea1e75513c408af5b30367f87d00826d1
Brandon Smith <bsmith@relaypro.com>
2022-07-29 14:01:36 -0400

    Suppress warning for using MD5

--- src/lib/user-id.ts
+++ src/lib/user-id.ts
@@ -5,7 +5,7 @@ import CryptoJS from 'crypto-js'
 
 export default function userId(subscriberId: string, userUuid: string): string {
   const base62 = Base62Str.createInstanceWithGmpCharacterSet()
-  const md5Bytes = Base62Str.getBytes(CryptoJS.enc.Latin1.stringify(CryptoJS.MD5(`${subscriberId}${userUuid}`)))
+  const md5Bytes = Base62Str.getBytes(CryptoJS.enc.Latin1.stringify(CryptoJS.MD5(`${subscriberId}${userUuid}`))) // lgtm[js/weak-cryptographic-algorithm]
   const encoded = Base62Str.getString(base62.encode(md5Bytes))
   return `VIRT${encoded}`
 }
