baae65a66b3cc6a5b5256b05cfa4576ef11da1ad
Brandon Smith <bsmith@relaypro.com>
2023-10-24 16:43:09 -0400

    Add `—process` flag to `relay env` command
    Will include any shell environment variables prefixed with `RELAY_`
    for better troubleshooting

--- src/commands/env.ts
+++ src/commands/env.ts
@@ -2,8 +2,10 @@
 
 import { CliUx } from '@oclif/core'
 import { Command } from '../lib/command'
-// import { string } from '../lib/flags'
+import * as flags from '../lib/flags'
 import { ROOT_DOMAIN, vars } from '../lib/vars'
+import pickBy from 'lodash/pickBy'
+import { startsWith } from 'lodash'
 
 type EnvResult = {
   env: string,
@@ -12,6 +14,8 @@ type EnvResult = {
   auth_cli_id: string,
   auth_sdk_id: string,
 }
+
+const PREFIX = `RELAY_`
 
 export default class Env extends Command {
   static enableJsonFlag = true
@@ -28,13 +32,31 @@ RELAY_HOST=all-main-pro-ibot.${ROOT_DOMAIN}  # default
 RELAY_HOST=all-main-qa-ibot.${ROOT_DOMAIN}
 `.trim()]
 
+  static flags = {
+
+    process: flags.boolean({
+      char: `P`,
+      required: false,
+      default: false,
+      description: `Include shell process environment variables`
+    }),
+  }
+
   async run(): Promise<EnvResult> {
-    const result = {
+    const { flags } = await this.parse(Env)
+    let result: EnvResult = {
       env: vars.rawEnv,
       api: vars.host,
       auth: vars.authHost,
       auth_cli_id: vars.authCliId,
       auth_sdk_id: vars.authSdkId,
+    }
+
+    if (flags.process) {
+      result = {
+        ...result,
+        ...pickBy(process.env, (_, v) => startsWith(v, PREFIX)),
+      }
     }
 
     if (!this.jsonEnabled()) {
