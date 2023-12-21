c4d65701d75485952e65c5e5ae1028fbba8c6a69
Ashley Godwin <agodwin@relaypro.com>
2023-12-21 11:48:50 -0500

    change a variable to camelcase

--- src/commands/task/schedule.ts
+++ src/commands/task/schedule.ts
@@ -78,19 +78,19 @@ export default class TasksScheduleCommand extends Command {
     const subscriberId = flags[`subscriber-id`]
     const scheduledArgs: ScheduleArgs = createScheduleArgs(argv)
 
-    let encoded_string = scheduledArgs.args as string
-    if (encoded_string.charAt(0) == `@`) {
+    let encodedString = scheduledArgs.args as string
+    if (encodedString.charAt(0) == `@`) {
 
-      const stats = fs.statSync(encoded_string.substring(1, encoded_string.length))
+      const stats = fs.statSync(encodedString.substring(1, encodedString.length))
       const fileSizeInMegabytes = stats.size / (1024*1024)
       if (fileSizeInMegabytes > 10) {
         this.error(`args file is too large`)
       }
 
-      encoded_string = fs.readFileSync(encoded_string.substring(1, encoded_string.length),{ encoding: `utf8`, flag: `r` }).toString()
+      encodedString = fs.readFileSync(encodedString.substring(1, encodedString.length),{ encoding: `utf8`, flag: `r` }).toString()
     }
 
-    const args = JSON.parse(encoded_string)
+    const args = JSON.parse(encodedString)
     args.tags = [scheduledArgs.type, ...(flags.tag ?? [])]
     scheduledArgs.args = args
 
--- src/commands/task/start.ts
+++ src/commands/task/start.ts
@@ -36,19 +36,19 @@ export default class TasksStartCommand extends Command {
     const subscriberId = flags[`subscriber-id`]
     const startArgs: StartArgs =  createStartArgs(argv)
 
-    let encoded_string = startArgs.args as string
-    if (encoded_string.charAt(0) == `@`) {
+    let encodedString = startArgs.args as string
+    if (encodedString.charAt(0) == `@`) {
 
-      const stats = fs.statSync(encoded_string.substring(1, encoded_string.length))
+      const stats = fs.statSync(encodedString.substring(1, encodedString.length))
       const fileSizeInMegabytes = stats.size / (1024*1024)
       if (fileSizeInMegabytes > 10) {
         this.error(`args file is too large`)
       }
 
-      encoded_string = fs.readFileSync(encoded_string.substring(1, encoded_string.length),{ encoding: `utf8`, flag: `r` }).toString()
+      encodedString = fs.readFileSync(encodedString.substring(1, encodedString.length),{ encoding: `utf8`, flag: `r` }).toString()
     }
 
-    const args = JSON.parse(encoded_string)
+    const args = JSON.parse(encodedString)
     args.tags = [startArgs.type, ...(flags.tag ?? [])]
     startArgs.args = args
 
